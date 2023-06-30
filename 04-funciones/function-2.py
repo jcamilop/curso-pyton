0e3565a97a7e3502964c32fc193906be2a7af471
jcamilop <https://github.com/jcamilop>
2023-06-30 15:29:50 -0500

    function

--- 04-funciones/function-2.py
+++ 04-funciones/function-2.py
@@ -9,3 +9,20 @@ greeting = greet('camilo')
 
 print(greeting)
 
+#suma
+
+def sumar(a, b):
+    return a + b
+suma = sumar(10, 50)
+
+print('the result:', suma)
+
+#resta
+
+def restar(a=None, b=None):
+    if a == None or b == None:
+        print('error')
+        return
+    return a - b
+resta = restar(b = 10, a = 20)
+print('the result:', resta)

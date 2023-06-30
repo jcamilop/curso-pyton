be82d39c497b42c4252ab8ce1d770bda40480a74
jcamilop <https://github.com/jcamilop>
2023-06-30 17:08:13 -0500

    lambdas

---
+++ 04-funciones/lambdas.py
@@ -0,0 +1,13 @@
+
+
+sumar = lambda a,b: a+b
+print(sumar(10, 20))
+
+restar = lambda a,b: a-b
+print(restar(100,20))
+
+par = lambda n: n % 2 == 0
+print(par(4))
+
+inpar = lambda n: n % 2 != 0
+print(inpar(5))

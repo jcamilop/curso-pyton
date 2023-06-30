b1539058a6bbc314372079112d72a69239d2d50e
jcamilop <https://github.com/jcamilop>
2023-06-30 16:47:20 -0500

    function

--- 04-funciones/fun_recursivas.py
+++ 04-funciones/fun_recursivas.py
@@ -1,10 +1,10 @@
 #las funciones recursivas se ejecutan asi mismas
 
 def factorial(n):
-    print('valor inicial:', n)
+    #print('valor inicial:', n)
     if n > 1:
         n = n * factorial(n - 1)
-    print('valor final:', n)
+    #print('valor final:', n)
     return n
 
 n = int(input('ingrese un numero: '))

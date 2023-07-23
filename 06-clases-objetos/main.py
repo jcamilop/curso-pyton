eacd18bfeac880f33de947dc008de979a72596c8
jcamilop <https://github.com/jcamilop>
2023-07-23 10:36:30 -0500

    class_objet

---
+++ 06-clases-objetos/main.py
@@ -0,0 +1,8 @@
+from objet import Persona
+
+persona1 = Persona()
+persona1.nombre ='camilo'
+persona1.edad =23
+persona1.ciudad ='medellin'
+
+persona1.dates()
---
+++ 06-clases-objetos/objet.py
@@ -0,0 +1,11 @@
+
+class Persona:
+    
+    nombre = None
+    edad = None
+    ciudad = None
+    
+    def dates(self):
+        print('nombre:',self.nombre)
+        print('edad:',self.edad)
+        print('ciudad:',self.ciudad)
